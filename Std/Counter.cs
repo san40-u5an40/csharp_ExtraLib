5f88747d513e48ce5fd560bd2f278d24cb1a3b5c
san40-u5an40 <alexandr.dev2011@gmail.com>
2025-10-03 14:53:58 +0300

    Рефакторинг класса Counter

--- Std/Counter.cs
+++ Std/Counter.cs
@@ -8,12 +8,15 @@ namespace Std;
 public class Counter
 {
     private int counter;
+    private string name;
 
     /// <summary>
-    /// Создаёт объект счётчика с указанным начальным значением
+    /// Создаёт объект счётчика с указанным начальным значением и названием
     /// </summary>
     /// <param name="startValue">Начальное значение счётчика</param>
-    public Counter(int startValue) => counter = startValue;
+    /// <param name="name">Название счётчика (по умолчанию: "Default")</param>
+    public Counter(int startValue, string name = "Default") =>
+        (this.counter, this.name) = (startValue, name);
 
     /// <summary>
     /// Значение счётчика
@@ -21,9 +24,34 @@ public class Counter
     public int Value => counter;
 
     /// <summary>
-    /// Метод, инкрементирующий значение счётчика
+    /// Название счётчика
     /// </summary>
-    public void Increment() => counter++;
+    public string Name => name;
+
+    /// <summary>
+    /// Метод, инкрементирующий значение счётчика, доступен к переопределению
+    /// </summary>
+    public virtual void Increment() => counter++;
+
+    // Группа переопределённых методов Object
+    public override string ToString() => $"{name}: {counter}";
+    public sealed override bool Equals(object? obj)
+    {
+        if (obj is Counter cnt)
+            return (this.Value, this.Name) == (cnt.Value, cnt.Name);
+
+        return false;
+    }
+    public sealed override int GetHashCode() => (Value, Name).GetHashCode();
+
+    // Группа переопределённых операторов
+    public static bool operator ==(Counter c1, Counter c2) => c1.Equals(c2);
+    public static bool operator !=(Counter c1, Counter c2) => !c1.Equals(c2);
+    public static Counter operator ++(Counter c1)
+    {
+        c1!.Increment();
+        return c1!;
+    }
 
     /// <summary>
     /// Возвращает функцию вызова счётчика с заданным начальным значением, реализованный при помощи замыкания
